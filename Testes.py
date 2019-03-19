63d41a725adc2e8aacfb78acc9c1cb2ce1233fbe
julianaibiapina <julianibiapina@alu.ufc.br>
2019-03-19 19:15:39 -0300

    Limpeza: criando coluna hashtags

--- Testes.py
+++ Testes.py
@@ -13,7 +13,7 @@ import getTwitterData
 #strip_handles=True -> irá remover mençoes a usuários do tweet
 #reduce_len=True -> irá reduzir caracteres duplicados
 #preserve_case=False -> irá alterar a forma da escrita, deixando tudo em minúsculo
-tweet_tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True, preserve_case=False)
+tweet_tokenizer = TweetTokenizer()
 
 list_words = ['governo']
 arquivo_csv = 'Governo02-Março-2019.csv'
@@ -32,12 +32,29 @@ dt_frame = pd.read_csv(arquivo_csv, encoding='UTF-8')
 #REMOVE TODAS AS LINHAS QUE POSSUEM TWEETS DUPLICADOS
 dt_frame = dt_frame.drop_duplicates('tweet')
 
+
+dt_frame['hashtags'] = 'null'
 #coloca todas as hashtags em uma nova coluna e as remove do tweet
-for index, row in dt_frame.iterrows() :
-    
+for index, row in dt_frame.iterrows():
+    tweet_tokens = tweet_tokenizer.tokenize(row['tweet'])
+    hashtags = list() #guarda todos as hashtags de um tweet em uma lista
+    for token in tweet_tokens:
+        if(token[0] == '#'):
+            hashtags.append(token)
+    #------------------------------------------> PAREI AQUI!!!! <-----------------------------------------
+    # COMO ATRIBUIR O VALOR REFERENTE A ESSA COLUNA NESSA LINHAS ESPECÍFICA.
+    # IDEIA: CRIAR UMA series E POVOAR ELA COM AS hashtags E DEPOIS CRIAR A COLUNA E ATRIBUIR ESSA series
+    #  dt_frame.loc[:, 'hashtags'] = seriesCriada.loc[:, 'A']
+    #Fonte: http://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html
+    dt_frame.iloc[index]['hashtags'] = hashtags
 
-
+    print(dt_frame.loc[index]['hashtags'])
 print('\n\n------------------------>Exclusão feita\n\n')
+
+
+
+#for index, row in dt_frame.iterrows():
+#    print(row['hashtags'])
 
 # transformar o DataFrame em .csv
 #dt_frame.to_csv('dadosLimpos.csv', encoding='utf-8', index=False)
