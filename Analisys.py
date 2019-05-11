f6c8e123d1a988fe26537da6faf5614f95e1363e
julianaibiapina <julianibiapina@alu.ufc.br>
2019-05-11 18:57:45 -0300

    Libgen-Muitos artigos e livro bons!!!

--- Analisys.py
+++ Analisys.py
@@ -1,45 +1,58 @@
-# Objetivo: armazenar em um dataframe os usuários e os seus respectivos seguidores.
+# Objetivo: armazenar em um .csv os usuários e os seus respectivos seguidores.
 import csv
 import pandas as pd
 import tweepy
+import time
 
 #ASSUNTO DESEJADO
 arquivo_csv = 'Data_30Abril-a-03Maio.csv'
 
 #pega a base de dados e transforma em dataframe
 dt_frame = pd.read_csv(arquivo_csv, encoding='ISO-8859-1')
-#print(dt_frame.info())
+print(dt_frame.info())
 
 # Usuários que comentaram sobre política durante o período de coleta
 users = dt_frame['user_screen_name'].drop_duplicates()
-print(type(users))
 
-# Armazena todos os amigos(aqueles a quem ele segue) de um usuário
-consumer_key = '8QtmEE1HWvcFlp5WoU7i5MNfk'
-consumer_secret = 'QCh2qo9ZW2p4lbTwjnjDpHUbmPPgk2Mi2aFpzeegVZH3PRZPPX'
-access_token = '1086082843476938753-cgDTEfWt3GxXJ2OtSvwOtS4DQ1GxeD'
-access_token_secret = 'KCUZvATUr1yibFVh8iaC0QWr5UKMbQnAWOGkzGBxRBV1U'
+
+# Armazena todos os seguidores de um usuário pertencente a base de dados
+
+consumer_key = 'u0W4cRMSNbJEaP2Bs5TWKcP86'
+consumer_secret = 'fqAsaJsxiIhSvrnIFXCa7Ro01yAhWW0RIf6uMjv3Bu1nl5UA0v'
+access_token = '1086082843476938753-ry4rjhmekNAJPPnYE6mp2D303wm3Yw'
+access_token_secret = 'xK4gPSqm0GNhoNpoT6A9Bedlv55w0lOmT9YuycjKHrySN'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# Esse processo demora devido ao litite imposto pela API do Twitter
+# Nome do arquivo onde os dados serão armazenados
+file_name = 'Usuario-Seguidores.csv'
+
+#cria o arquivo
+csvData = open(file_name, 'w')
+
+#cria csv writer
+writer = csv.writer(csvData)
+
+#cria uma linha com os nomes de cada coluna
+writer.writerow(['usuário', 'seguidores'])
+
+# Esse processo demora devido ao limite imposto pela API do Twitter
+# https://developer.twitter.com/en/docs/basics/rate-limiting
 for user in users:
+    #ids = list()
+    seguidores = ''
     print('Usuário atual: %s' % (user))
-    user = api.get_user(screen_name=user)
-    print('Amigos:')
-    for friend in tweepy.Cursor(api.friends).items():
-        print('\t %s' % (friend.screen_name))
-# Deve estar dentro do laço para
-# user = api.get_user(screen_name='Ju_Ibiapina')
-# print ("My Twitter Handle: " +  str(user.screen_name))
-# ct = 0
-#
-# for friend in tweepy.Cursor(api.friends).items():
-#     # Process the friend here
-#     print(friend.screen_name)
-#     ct += 1
-#
-# print ("\n\nFinal Count: " + str(ct))
+    for page in tweepy.Cursor(api.followers, screen_name=user).pages():
+        status = page[0]
+        print(status.screen_name)
+        print('\n')
+        seguidores = seguidores + status.screen_name + "/"
+        #ids.append(status.screen_name)
+    writer.writerow([user, seguidores])
+
+print("Dados coletados com sucesso.")
+# Fecha o arquivo
+csvData.close()
