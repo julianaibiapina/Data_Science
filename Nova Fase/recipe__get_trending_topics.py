7667948d9265ee7ada9e549b9573e8427f9a17f1
julianaibiapina <julianibiapina@alu.ufc.br>
2019-05-14 16:50:16 -0300

    atualização: + livros

---
+++ Nova Fase/recipe__get_trending_topics.py
@@ -0,0 +1,11 @@
+
+import json
+import twitter
+from recipe__oauth_login import oauth_login
+
+t = oauth_login()
+WORLD_WOE_ID = 1 # The Yahoo! Where On Earth ID for the entire world
+world_trends = t.trends.place(_id=WORLD_WOE_ID) # get back a callable
+
+# call the callable and iterate through the trends returned
+print (json.dumps(world_trends, indent=1))
